05c410bc0a8e06821611ffd4182a89efd416b740
deaddog <mikkelslsl@gmail.com>
2014-11-04 21:43:53 +0100

    Created a template selector and multiply data templates for the votes listbox.

---
+++ WpfPlanning/VoteDataTemplateSelector.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfPlanning
+{
+    public class VoteDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item != null && item is WpfVote)
+            {
+                WpfVote vote = item as WpfVote;
+
+                Window window = System.Windows.Application.Current.MainWindow;
+                ListBox list = window.FindName("votes") as ListBox;
+
+                bool all = true;
+                foreach (WpfVote obj in list.Items)
+                    if (!obj.VoteType.HasValue)
+                    {
+                        all = false;
+                        break;
+                    }
+
+                if (all)
+                    return list.FindResource("VisibleVote") as DataTemplate;
+                else if (vote.VoteType.HasValue)
+                    return list.FindResource("HiddenVote") as DataTemplate;
+                else
+                    return list.FindResource("NoVote") as DataTemplate;
+            }
+
+            return null;
+        }
+    }
+}
